ff6363f9cdcbf99e203127a4b2bd5787c72c1430
Jessie Kong <jessiekong@Jessies-MacBook-Air-7.local>
2025-04-27 21:22:27 -0700

    Updated

--- LibBook.py
+++ LibBook.py
@@ -10,6 +10,21 @@ Data Strucutres Used: Linked List, Heap/Priority Queue
 import heapq
 
 # --- Book Class ---
+class Book:
+    def __init__(self, title, author, dewey_decimal):
+        self.title = title
+        self.author = author
+        self.dewey_decimal = dewey_decimal
+        self.is_available = True
+        self.reservation_queue = [] # prio queue of (prio, username)
+
+    def reserve_book(self, username, prioprity=0):
+        heapq.heappush(self.reservation_queue, (priority, username))
+
+    def next_reservation(self):
+        if self.reservation_queue:
+            return heapq.heappop(self.rservation_queue)
+        return None
 
 # --- Linked List Node for User History ---
 
