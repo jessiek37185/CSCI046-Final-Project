961ce5250a6122d3536b09326b38f4ba39b09454
Jessie Kong <jessiekong@Jessies-MacBook-Air-7.local>
2025-04-27 21:10:45 -0700

    updated homepage

---
+++ LibBook.py
@@ -0,0 +1,18 @@
+"""
+Library Book Management System
+
+Design a system that allows users to check out, return and search for books in library.
+The system should keep track of book availability and allow efficient retrieval. 
+
+Data Strucutres Used: Linked List, Heap/Priority Queue
+"""
+
+import heapq
+
+# --- Book Class ---
+
+# --- Linked List Node for User History ---
+
+# --- User Class ---
+
+# --- Library System Class ---
